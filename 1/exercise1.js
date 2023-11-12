3bf5fb4f14fb5dd3394f9e59881f6e7fb111930e
Janett Venegas <jannv25@gmail.com>
2023-11-12 17:45:12 -0600

    correction

--- 1/exercise1.js
+++ 1/exercise1.js
@@ -1,4 +1,6 @@
-/*Exercise #1 (sugerencia: join())
+/*
+Exercise #1 
+(sugerencia: join())
 Copy the code below into your workspace:
 let arr = ["This", "is", "a", "sentence."];
 function printOutString() {
@@ -9,12 +11,15 @@ printOutString();
 Complete the function to print out the string: This is a sentence.
 */
 
-const arr = ["This", "is", "a", "sentence."]; //Se declara el arreglo
-function printOutString() {  //Defines una función
-   console.log(arr.join(" ")); //utilizas el método  join  para concatenar los elementos del arreglo en una sola cadena
+/*Solución con el metodo join() en javaScript se utiliza para combinar todos los elementos de un arreglo en una sola cadena y devolver esa cadena como resultado. Este método toma un argurmento opcional que especifica el separador que se utilizará para unir los elemntos del arreglo. */
+
+const array = ["This", "is", "a", "sentence."]; //Se declara el arreglo
+function printOutString(array) {  //Defines una función
+  return array.join(" ");  //utilizas el método  join  para combinar los elementos del arreglo en una sola cadena
 }
 
-printOutString(); //llamas a la función
+console.log(printOutString(array)); //llamas a la función que unio a todos los elementos
+
 
 
 
